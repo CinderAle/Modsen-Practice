9a313d6a3daf58dde984d24e2dbf94051f905315
CinderAle <a.d.strukov@gmail.com>
2024-06-24 18:19:20 +0300

    Test task added styled components for GMap

---
+++ Test-Task/src/components/GMap/StyledPlaceMarker.tsx
@@ -0,0 +1,21 @@
+import { MarkerF } from "@react-google-maps/api";
+import { LatLng, LatLngLiteral } from "google-maps-react-markers";
+
+interface Props {
+  coordinates: LatLngLiteral | LatLng;
+}
+
+const StyledPlaceMarker = ({ coordinates }: Props) => {
+  return (
+    <MarkerF
+      position={coordinates}
+      icon={{
+        url: "/places/car.svg",
+        scaledSize: new google.maps.Size(30, 30),
+        anchor: new google.maps.Point(15, 15),
+      }}
+    />
+  );
+};
+
+export default StyledPlaceMarker;
---
+++ Test-Task/src/components/GMap/StyledSearchCircle.tsx
@@ -0,0 +1,29 @@
+import { CircleF } from "@react-google-maps/api";
+import { LatLngLiteral } from "google-maps-react-markers";
+import { CIRCLE_RGB_COLOR } from "./constants";
+
+interface Props {
+  center: LatLngLiteral;
+  radius?: number;
+}
+
+const OPACITY = 0.1;
+const STROKE_WIDTH = 2;
+
+const StyledSearchCircle = ({ center, radius }: Props) => {
+  return (
+    <CircleF
+      center={center}
+      radius={radius}
+      options={{
+        strokeColor: CIRCLE_RGB_COLOR,
+        fillColor: CIRCLE_RGB_COLOR,
+        strokePosition: google.maps.StrokePosition.INSIDE,
+        strokeWeight: STROKE_WIDTH,
+        fillOpacity: OPACITY,
+      }}
+    />
+  );
+};
+
+export default StyledSearchCircle;
