e15142fb9c580038e51ac1b1725b5a46861bdeb7
CinderAle <a.d.strukov@gmail.com>
2024-07-03 01:00:06 +0300

    fix: changed intefaces to types

--- Test-Task/src/store/reducers/zoomReducer.ts
+++ Test-Task/src/store/reducers/zoomReducer.ts
@@ -1,28 +1,36 @@
-import { ZOOM_CONSTANTS } from "@/constants/zoomConstants"
+import { ZOOM_CONSTANTS } from "@/constants/zoomConstants";
 
 export enum ZoomActionType {
-    Increase = "INCREASE", Decrease = "DECREASE"
+    Increase = "INCREASE",
+    Decrease = "DECREASE",
 }
 
-export interface ZoomAction {
-    type: ZoomActionType
-}
+export type ZoomAction = {
+    type: ZoomActionType;
+};
 
-interface ZoomState {
-    value: number
-}
+type ZoomState = {
+    value: number;
+};
 
 const initialState: ZoomState = {
-    value: ZOOM_CONSTANTS.DEFAULT_ZOOM
-}
+    value: ZOOM_CONSTANTS.DEFAULT_ZOOM,
+};
 
-export const zoomReducer = (state: ZoomState = initialState, action: ZoomAction) : ZoomState => {
-    switch(action.type) {
+export const zoomReducer = (
+    state: ZoomState = initialState,
+    action: ZoomAction,
+): ZoomState => {
+    switch (action.type) {
         case ZoomActionType.Increase:
-            return state.value === ZOOM_CONSTANTS.MAX_ZOOM ? state : {value: state.value + 1};
+            return state.value === ZOOM_CONSTANTS.MAX_ZOOM
+                ? state
+                : { value: state.value + 1 };
         case ZoomActionType.Decrease:
-            return state.value === ZOOM_CONSTANTS.MIN_ZOOM ? state : {value: state.value - 1};
+            return state.value === ZOOM_CONSTANTS.MIN_ZOOM
+                ? state
+                : { value: state.value - 1 };
         default:
             return state;
     }
-}
+};
